6d37dfa375dd092435ce84ef99baf02388f6c830
Luã Álvaro Lage Carlos <lua.alvaro@ubistart.com>
2022-06-20 18:18:49 -0300

    Alteração do nome e forçar deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header({ toggleFormView, formView }) {
     return (
         <header className="header">
-            <h2 className="header-logo">Finanças</h2>
+            <h2 className="header-logo">La Finances</h2>
             <button className="header-button" onClick={toggleFormView}>{formView ? 'FECHAR' : 'ADICIONAR'}</button>
         </header>
     )
