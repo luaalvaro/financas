b38a4acd73348db61c027bb2ef420f68cb8e287b
Luã Álvaro Lage Carlos <lua.alvaro@ubistart.com>
2022-06-20 18:09:19 -0300

    Ordenação por data

--- src/App.js
+++ src/App.js
@@ -36,6 +36,12 @@ function App() {
     localStorage.setItem('FinancasStorageData', JSON.stringify(data))
   }, [data])
 
+  console.log(data)
+
+  data.sort((a, b) => {
+    return new Date(a.paymentDate) - new Date(b.paymentDate)
+  })
+
   return (
     <>
       <Header toggleFormView={toggleFormView} formView={formView} />
